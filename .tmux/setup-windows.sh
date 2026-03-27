50d64b636e35965c3be1f177c841a58a22089afc
Malcolm Brown <malcolm.brown@brcon.net>
2026-03-26 20:25:31 -0400

    updates

--- .tmux/setup-windows.sh
+++ .tmux/setup-windows.sh
@@ -7,8 +7,6 @@ if tmux has-session -t "$SESSION" 2>/dev/null; then
 else 
     tmux new-session -d -s "$SESSION" -n 'main'
 
-    tmux select-window -t "$SESSION:main":
-    
     tmux attach-session -t "$SESSION"
 fi 
 
